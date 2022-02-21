18dd194d79903d5664adc6be629cdc67212021a0
Juan Chango <juan.chango_ex@scotiabank.cl>
2022-02-21 11:01:36 -0300

    Initial Commit

---
+++ swiftcoin/swiftcoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftcoinApp.swift
+//  swiftcoin
+//
+//  Created by usuario on 21-02-22.
+//
+
+import SwiftUI
+
+@main
+struct swiftcoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
