6cc14df565bbe659098dc7de0f8e252fab0dbd74
Juan Chango <jnfran92@gmail.com>
2022-02-23 18:01:55 -0300

    working on cocoa pods

--- Podfile
+++ Podfile
@@ -5,3 +5,9 @@ target 'swiftcoin' do
     pod 'RxSwift', '6.5.0'
     pod 'RxCocoa', '6.5.0'
 end
+
+# RxTest and RxBlocking make the most sense in the context of unit/integration tests
+target 'swiftcoinTests' do
+    pod 'RxBlocking', '6.5.0'
+    pod 'RxTest', '6.5.0'
+end
--- swiftcoin/view/cryptolist/presentation/CryptoListViewModel.swift
+++ swiftcoin/view/cryptolist/presentation/CryptoListViewModel.swift
@@ -19,11 +19,13 @@ class CryptoListViewModel: ObservableObject {
     func loadData(){
         print("loadData START")
         self.uiState = .defaultView
-//        getCryptosData()
+        Task{
+            try await self.uiState = .showDataView(getCryptosData())
+        }
     }
     
     func getCryptosData() async throws -> [UICrypto]{
-        var cryptoList = [
+        let cryptoList = [
             UICrypto(id: 0, name: "Bticoin Lite", symbol: "BTC", price: "$100.00"),
             UICrypto(id: 1, name: "Ethreum", symbol: "ETH", price: "$200.00"),
             UICrypto(id: 2, name: "LiteCoin", symbol: "LTC", price: "$300.00"),
