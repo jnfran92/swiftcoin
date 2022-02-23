5eadbc15f3d074fea99a21abbbf163ffbb227521
Juan Chango <jnfran92@gmail.com>
2022-02-23 17:48:06 -0300

    some code cleanup

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Podfile
+use_frameworks!
+
+target 'swiftcoin' do
+    pod 'RxSwift', '6.5.0'
+    pod 'RxCocoa', '6.5.0'
+end
--- swiftcoin/view/cryptolist/presentation/CryptoListViewModel.swift
+++ swiftcoin/view/cryptolist/presentation/CryptoListViewModel.swift
@@ -9,15 +9,6 @@ import Foundation
 
 class CryptoListViewModel: ObservableObject {
     
-    var cryptoList = [
-        UICrypto(id: 0, name: "Bticoin Lite", symbol: "BTC", price: "$100.00"),
-        UICrypto(id: 1, name: "Ethreum", symbol: "ETH", price: "$200.00"),
-        UICrypto(id: 2, name: "LiteCoin", symbol: "LTC", price: "$300.00"),
-        UICrypto(id: 3, name: "Bticoin Lite", symbol: "BTC", price: "$100.00"),
-        UICrypto(id: 4, name: "Ethreum", symbol: "ETH", price: "$200.00"),
-        UICrypto(id: 5, name: "LiteCoin", symbol: "LTC", price: "$300.00")
-    ]
-        
     @Published var uiState: CryptoListUIState = .defaultView
     
     init() {
@@ -28,9 +19,20 @@ class CryptoListViewModel: ObservableObject {
     func loadData(){
         print("loadData START")
         self.uiState = .defaultView
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            print("loadData DONE")
-            self.uiState = .showDataView(self.cryptoList)
-        }
+//        getCryptosData()
     }
+    
+    func getCryptosData() async throws -> [UICrypto]{
+        var cryptoList = [
+            UICrypto(id: 0, name: "Bticoin Lite", symbol: "BTC", price: "$100.00"),
+            UICrypto(id: 1, name: "Ethreum", symbol: "ETH", price: "$200.00"),
+            UICrypto(id: 2, name: "LiteCoin", symbol: "LTC", price: "$300.00"),
+            UICrypto(id: 3, name: "Bticoin Lite", symbol: "BTC", price: "$100.00"),
+            UICrypto(id: 4, name: "Ethreum", symbol: "ETH", price: "$200.00"),
+            UICrypto(id: 5, name: "LiteCoin", symbol: "LTC", price: "$300.00")
+        ]
+        try await Task.sleep(nanoseconds: 2 * 1_000_000_000)
+        return cryptoList
+    }
+    
 }
