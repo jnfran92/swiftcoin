e1d5d43530911e999c4565866bfb3e9df0a1d90d
Juan Chango <jnfran92@gmail.com>
2022-03-01 17:39:00 -0300

    working on view model

--- swiftcoin/view/cryptolist/presentation/CryptoListViewModel.swift
+++ swiftcoin/view/cryptolist/presentation/CryptoListViewModel.swift
@@ -7,17 +7,22 @@
 
 import Foundation
 import Alamofire
+import Combine
+import SwiftUI
 
 @MainActor
 class CryptoListViewModel: ObservableObject {
     
     @Published var uiState: CryptoListUIState = .defaultView
     
+    private var cancellableSet: Set<AnyCancellable> = []
+    
     init() {
         print("CryptoListViewModel init")
-        Task{
-            try await loadData()
-        }
+        loadData3()
+        //        Task{
+        //            try await loadData()
+        //        }
     }
     
     deinit {
@@ -38,6 +43,35 @@ class CryptoListViewModel: ObservableObject {
         }
     }
     
+    
+    func loadData2(){
+        print("loadData2")
+        self.uiState = .showLoadingView
+        getDataFromCloud2().sink { dataResponse in
+            if dataResponse.error != nil {
+                self.uiState = .showErrorView
+            } else {
+                let data = dataResponse.value!.data!.enumerated().map{ (idx, it) -> UICrypto in
+                    UICrypto(id: it.cryptoId ?? 0, name: it.name ?? "", symbol: it.symbol ?? "", price: "\(it.quote?.usd?.price ?? 0.0)")
+                }
+                self.uiState = .showDataView(data)
+            }
+        }.store(in: &cancellableSet)
+    }
+    
+    func loadData3(){
+        print("loadData3")
+        self.uiState = .showLoadingView
+        getDataFromCloud3()
+            .sink(receiveCompletion: { data in
+                print(data)
+            }, receiveValue: { data in
+                print(data)
+            })
+            .store(in: &cancellableSet)
+    }
+    
+    
     enum ViewModelError: Error {
         case runtimeError(String)
     }
@@ -46,8 +80,9 @@ class CryptoListViewModel: ObservableObject {
     let headers: HTTPHeaders = [
         "X-CMC_PRO_API_KEY": "bacdbc14-d7d9-4a0c-8ec5-77351a6be042"
     ]
+    let url = URL(string: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest")!
     
-
+    
     func getDataFromCloud() async throws -> CryptoApiRequest {
         try await withUnsafeThrowingContinuation { continuation in
             AF.request("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", headers: headers).responseDecodable(of: CryptoApiRequest.self) { (response) in
@@ -64,6 +99,38 @@ class CryptoListViewModel: ObservableObject {
                 fatalError("should not get here")
             }
         }
+    }
+    
+    
+    func getDataFromCloud2() -> AnyPublisher<DataResponse<CryptoApiRequest, CryptoListViewModel.ViewModelError>, Never> {
+        return AF.request(url, headers: headers)
+            .validate()
+            .publishDecodable(type: CryptoApiRequest.self)
+            .map{ response in
+                response.mapError { error in
+                    return ViewModelError.runtimeError("error: \(error)")
+                }
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    func getDataFromCloud3() -> AnyPublisher<CryptoApiRequest, CryptoListViewModel.ViewModelError> {
+        return AF.request(url, headers: headers)
+            .validate()
+            .publishDecodable(type: CryptoApiRequest.self)
+            .tryMap{ dataResponse in
+                if dataResponse.error != nil {
+                    throw ViewModelError.runtimeError("error:")
+                } else {
+                    return dataResponse.value!
+                }
+            }
+            .mapError{ error in
+                ViewModelError.runtimeError("error: \(error)")
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
     }
     
     func cancel(){
