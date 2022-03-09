81cde70e57cfdba1387e2ca49abee46386e93eb9
Juan Chango <jnfran92@gmail.com>
2022-03-09 12:04:29 -0300

    working on combine

--- swiftcoin/data/crypto/source/local/CryptoLocalSource.swift
+++ swiftcoin/data/crypto/source/local/CryptoLocalSource.swift
@@ -8,19 +8,30 @@
 import Foundation
 import RealmSwift
 import UIKit
+import Combine
 
 
 struct CryptoLocalSource {
     
     let realm: Realm
     
-    func addCrypto() {
-        print("addCrypto")
+    func addCrypto(item: DataCrypto) -> AnyPublisher<DataCrypto, AppError> {
+        let subject = PassthroughSubject<DataCrypto, AppError>()
+        
         let item = LocalCrypto()
         item.name = "Bitcoin2"
         try! realm.write({
             realm.add(item)
         })
+    }
+    
+    func writeCryptoToDB(item: LocalCrypto) -> Future<Bool, AppError>{
+        Future{  promise in
+            try! realm.write({
+                realm.add(item)
+            })
+            promise(true)
+        }
     }
     
     
