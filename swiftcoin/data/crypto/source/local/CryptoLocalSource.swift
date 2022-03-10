f7f7b7f5f24456ebc156f44c4dc5894710db0e81
Juan Chango <jnfran92@gmail.com>
2022-03-10 11:32:22 -0300

    realm added to crypto with combine and futures

--- swiftcoin/data/crypto/source/local/CryptoLocalSource.swift
+++ swiftcoin/data/crypto/source/local/CryptoLocalSource.swift
@@ -15,30 +15,33 @@ struct CryptoLocalSource {
     
     let realm: Realm
     
-    func addCrypto(item: DataCrypto) -> AnyPublisher<DataCrypto, AppError> {
-        let subject = PassthroughSubject<DataCrypto, AppError>()
-        
-        let item = LocalCrypto()
-        item.name = "Bitcoin2"
-        try! realm.write({
-            realm.add(item)
-        })
+    func addCrypto(item: DataCrypto) -> AnyPublisher<Bool, AppError> {
+        return Deferred{
+            Future{promise in
+                do{
+                    let localCrypto = LocalCrypto()
+                    localCrypto.name = item.name
+                    try realm.write({
+                        realm.add(localCrypto)
+                    })
+                    promise(.success(true))
+                }catch{
+                    promise(.failure(AppError.dataError("Error on write Realm local crypto")))
+                }
+            }
+        }.eraseToAnyPublisher()
     }
     
-    func writeCryptoToDB(item: LocalCrypto) -> Future<Bool, AppError>{
-        Future{  promise in
-            try! realm.write({
-                realm.add(item)
-            })
-            promise(true)
-        }
-    }
-    
-    
-    func getCryptoList(){
-        print("getCryptoList")
-        let items = realm.objects(LocalCrypto.self)
-//        let data = items.
-        print("getCryptoList items \(items)")
+    func getCryptoList() -> AnyPublisher<[DataCrypto], AppError>{
+        return Deferred{
+            Future{promise in
+                let items = Array(realm.objects(LocalCrypto.self).map{$0})
+                    .map { item in
+                    DataCrypto(
+                        id: 0, name: item.name, symbol: item.symbol, slug: item.slug, usdPrice: DataPrice(price: item.usdPrice?.price ?? 0.0, marketCap: item.usdPrice?.price ?? 0.0, volume24h: item.usdPrice?.price ?? 0.0, percentChange1h: item.usdPrice?.price ?? 0.0, percentChange24h: item.usdPrice?.price ?? 0.0, percentChange7d: item.usdPrice?.price ?? 0.0, lastUpdated: item.usdPrice?.lastUpdated ?? ""), tags: [], cmcRank: item.cmcRank, circulatingSupply: item.circulatingSupply, totalSupply: item.totalSupply, maxSupply: item.maxSupply)
+                }
+                promise(.success(items))
+            }
+        }.eraseToAnyPublisher()
     }
 }
