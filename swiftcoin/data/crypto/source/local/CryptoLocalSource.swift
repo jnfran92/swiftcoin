9f46069e9a85bd29a653f66c5ea6c5dc503a2257
Juan Chango <jnfran92@gmail.com>
2022-03-10 18:27:08 -0300

    repository working local and remote, some issues should be fixed yet

--- swiftcoin/data/crypto/source/local/CryptoLocalSource.swift
+++ swiftcoin/data/crypto/source/local/CryptoLocalSource.swift
@@ -43,13 +43,14 @@ struct CryptoLocalSource {
     
     func getCryptoList() -> AnyPublisher<[DataCrypto], AppError>{
         print("getCryptoList")
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
         return Deferred{
             Future{promise in
                 print("Future and promise getCrypto")
                 let items = Array(realm.objects(LocalCrypto.self).map{$0})
                     .map { item in
                         DataCrypto(
-                            id: 0, name: item.name, symbol: item.symbol, slug: item.slug, usdPrice: DataPrice(price: item.usdPrice?.price ?? 0.0, marketCap: item.usdPrice?.price ?? 0.0, volume24h: item.usdPrice?.price ?? 0.0, percentChange1h: item.usdPrice?.price ?? 0.0, percentChange24h: item.usdPrice?.price ?? 0.0, percentChange7d: item.usdPrice?.price ?? 0.0, lastUpdated: item.usdPrice?.lastUpdated ?? ""), tags: [], cmcRank: item.cmcRank, circulatingSupply: item.circulatingSupply, totalSupply: item.totalSupply, maxSupply: item.maxSupply)
+                            id: Int.random(in: 1..<1000), name: item.name, symbol: item.symbol, slug: item.slug, usdPrice: DataPrice(price: item.usdPrice?.price ?? 0.0, marketCap: item.usdPrice?.price ?? 0.0, volume24h: item.usdPrice?.price ?? 0.0, percentChange1h: item.usdPrice?.price ?? 0.0, percentChange24h: item.usdPrice?.price ?? 0.0, percentChange7d: item.usdPrice?.price ?? 0.0, lastUpdated: item.usdPrice?.lastUpdated ?? ""), tags: [], cmcRank: item.cmcRank, circulatingSupply: item.circulatingSupply, totalSupply: item.totalSupply, maxSupply: item.maxSupply)
                     }
                 promise(.success(items))
             }
